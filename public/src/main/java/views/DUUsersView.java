6fa8f9289f41216a17eeac886c30953e36f406ea
DmitryKuzin <mr.kuzinio@gmail.com>
2016-05-25 07:15:24 +0300

    njvnj

--- public/src/main/java/views/DUUsersView.java
+++ public/src/main/java/views/DUUsersView.java
@@ -1,8 +1,10 @@
 package views;
 
 import ru.kpfu.itis.domain.Users;
+import ru.kpfu.itis.wrappers.UsersWrapper;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,19 +12,23 @@ import java.util.List;
  */
 public class DUUsersView implements Serializable {
 
-    public List<Users> users;
+    public List<UsersWrapper> users;
 
     public DUUsersView(){}
 
-    public DUUsersView(List<Users> users) {
-        this.users = users;
+    public DUUsersView(List<Users> users)
+    {
+        this.users=new ArrayList<UsersWrapper>();
+        for(Users u:users){
+            this.users.add(new UsersWrapper(u));
+        }
     }
 
-    public List<Users> getUsers() {
+    public List<UsersWrapper> getUsers() {
         return users;
     }
 
-    public void setUsers(List<Users> users) {
+    public void setUsers(List<UsersWrapper> users) {
         this.users = users;
     }
 }
